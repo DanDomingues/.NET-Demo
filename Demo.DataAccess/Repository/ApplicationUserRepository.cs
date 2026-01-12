a5f84509945ca53632351bf2c60585865a9cce78
Dan Domingues <dom.dev.contact@gmail.com>
2026-01-12 00:53:27 -0300

    Added ApplicationUserRepository

---
+++ Demo.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Demo.DataAccess.Data;
+using Demo.DataAccess.Repository.IRepository;
+using Demo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DataAccess.Repository
+{
+    public class ApplicationUserRepository(DbSet<ApplicationUser> set) : Repository<ApplicationUser>(set), IApplicationUserRepository
+    {
+    }
+}
