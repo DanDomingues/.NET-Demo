2618e51bb2fb976336fd5be111f5b0ce1f0715b2
Dan Domingues <dom.dev.contact@gmail.com>
2026-01-29 20:28:13 -0300

    Updated IdentityUser references to enable Identity classes

--- Demo.Models/obj/Debug/net8.0/Demo.Models.AssemblyInfo.cs
+++ Demo.Models/obj/Debug/net8.0/Demo.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d91f0f73328f1a65f5ed4908483a98500cb59c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75aca75b5545eaa24f8e6d5aa9b35fcac1821b06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
