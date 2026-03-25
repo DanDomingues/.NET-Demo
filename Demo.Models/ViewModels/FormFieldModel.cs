573f21a8396abee5d524a2b1a7c1805862a40f1b
Dan Domingues <dom.dev.contact@gmail.com>
2026-03-25 01:32:07 -0300

    Improved label assignment in Form Field Model

--- Demo.Models/ViewModels/FormFieldModel.cs
+++ Demo.Models/ViewModels/FormFieldModel.cs
@@ -1,16 +1,35 @@
+using System.Text.RegularExpressions;
+
 namespace Demo.Models.ViewModels
 {
-    public class FormFieldModel(
-        string fieldName,
-        string? value,
-        string? label = null,
-        bool requiresRoleValidation = true,
-        bool isReadOnly = false)
+    public class FormFieldModel
     {
-        public string FieldName { get; set; } = fieldName;
-        public string? Label { get; set; } = label;
-        public string? Value { get; set; } = value;
-        public bool RequiresRoleValidation { get; set; } = requiresRoleValidation;
-        public bool IsReadOnly { get; set; } = isReadOnly;
+        public string FieldName { get; set; }
+        public string? Label { get; set; }
+        public string? Value { get; set; }
+        public bool RequiresRoleValidation { get; set; }
+        public bool IsReadOnly { get; set; }
+
+        public FormFieldModel(
+            string fieldName,
+            string? value,
+            string? label = null,
+            bool requiresRoleValidation = true,
+            bool isReadOnly = false)
+        {
+            FieldName = fieldName;
+            Value = value;
+            
+            if(string.IsNullOrEmpty(label))
+            {
+                label = fieldName.Split('.').Last();
+                label = Regex.Replace(label, @"(?<!^)([A-Z])", " $1");
+            }
+            
+            Label = label;
+
+            RequiresRoleValidation = requiresRoleValidation;
+            IsReadOnly = isReadOnly;
+        }
     }
 }
