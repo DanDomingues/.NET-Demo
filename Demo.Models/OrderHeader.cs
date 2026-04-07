c601eda477f82a5bf5b12bee359423a528472e6d
Dan Domingues <dom.dev.contact@gmail.com>
2026-04-07 03:19:27 -0300

    Minor tweaks to Order Details page

--- Demo.Models/OrderHeader.cs
+++ Demo.Models/OrderHeader.cs
@@ -11,24 +11,33 @@ namespace Demo.Models
 {
     public class OrderHeader : ModelBase
     {
+        [Display(Name = "Order Date")]
         public DateTime OrderDate { get; set; }
+        [Display(Name = "Shipping Date")]
         public DateTime ShippingDate { get; set; }
+        [Display(Name = "Payment Date")]
         public DateTime PaymentDate { get; set; }
+        [Display(Name = "Payment Due Date")]
         public DateOnly PaymentDueDate { get; set; }
+        [Display(Name = "Order Total")]
         public double OrderTotal { get; set; }
 
+
+        [Display(Name = "Order Status")]
         public string? OrderStatus { get; set; } = null!;
+        [Display(Name = "Payment Status")]
         public string? PaymentStatus { get; set; } = null!;
+        [Display(Name = "Tracking Number")]
         public string? TrackingNumber { get; set; }
         public string? Carrier { get; set; }
 
-        [Required] public string PhoneNumber { get; set; } = null!;
-        [Required] public string StreetAddress { get; set; } = null!;
+        [Required, Display(Name = "Phone Number")] public string PhoneNumber { get; set; } = null!;
+        [Required, Display(Name = "Street Address")] public string StreetAddress { get; set; } = null!;
         [Required] public string City { get; set; } = null!;
         [Required] public string State { get; set; } = null!;
-        [Required] public string PostalCode { get; set; } = null!;
-        [Required] public string FirstName { get; set; } = null!;
-        [Required] public string LastName { get; set; } = null!;
+        [Required, Display(Name = "Postal Code")] public string PostalCode { get; set; } = null!;
+        [Required, Display(Name = "First Name")] public string FirstName { get; set; } = null!;
+        [Required, Display(Name = "Last Name")] public string LastName { get; set; } = null!;
 
         public string? SessionId { get; set; }
         public string? PaymentIntentId { get; set; }
@@ -37,6 +46,7 @@ namespace Demo.Models
         [ForeignKey("ApplicationUserId"), ValidateNever]
         public ApplicationUser ApplicationUser { get; set; } = null!;
 
+        [Display(Name = "Full Name")]
         public string FullName => $"{FirstName} {LastName}";
     }
 }
