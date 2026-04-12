71b80f864987acaa07b0db56faba933ff580b616
Dan Domingues <dom.dev.contact@gmail.com>
2026-04-12 17:39:05 -0300

    Restricted certain order columns at index to admin versions

--- Northstar.Web/wwwroot/js/order.js
+++ Northstar.Web/wwwroot/js/order.js
@@ -5,17 +5,51 @@ $(document).ready(function ()
     var url = new URLSearchParams(window.location.search);
     var userFilter = url.get("filter");
     var statusFilter = url.get("status") ?? "all";
-    loadDataTable({ status: statusFilter, filter: userFilter });
+    var isAdmin = statusFilter === "all";
+
+    loadDataTable({ status: statusFilter, filter: userFilter }, isAdmin);
 });
 
-function loadDataTable(options) {
+function loadDataTable(options, isAdmin) {
+    var columns = [
+        { data: 'id' }
+    ]
+
+    if(isAdmin)
+    {
+        columns.push(
+            { data: 'fullName' },
+            { data: 'phoneNumber', className: 'text-start' },
+            { data: 'applicationUser.email' });
+    }
+
+    columns.push(
+        { data: 'orderDate', render: renderDateTime },
+        { data: 'orderStatus' },
+        { data: 'paymentStatus' },
+        { data: 'orderTotal', render: renderCurrency },
+        { 
+            data: 'id', 
+            width: '8%', 
+            className: 'text-end', 
+            render: function (data) 
+            { 
+                return renderButtons(data, options.filter) 
+            } 
+        }
+    )
+
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/customer/order/getallby?status=' + options.status + '&filter=' + options.filter },
+        "columns": columns
+    });
+}
+
+function loadCustomerDataTable(options) {
     dataTable = $('#tblData').DataTable({
         "ajax": { url: '/customer/order/getallby?status=' + options.status + '&filter=' + options.filter },
         "columns": [
             { data: 'id' },
-            { data: 'fullName' },
-            { data: 'phoneNumber', className: 'text-start' },
-            { data: 'applicationUser.email' },
             { data: 'orderDate', render: renderDateTime },
             { data: 'orderStatus' },
             { data: 'paymentStatus' },
@@ -35,8 +69,6 @@ function loadDataTable(options) {
 
 function renderButtons(id, filter) 
 {
-    //TODO: Validate this as an icon and remove below lines (and css) when done
-    //var buttonClass = isAdmin ? 'admin-table-action' : 'northstar-table-action';
     var isAdmin = document.body.classList.contains('admin-ops');
     var iconClass = isAdmin ? 'bi-pencil-square' : 'bi-eye'
     var label = isAdmin ? 'Edit Order' : 'View Order';
